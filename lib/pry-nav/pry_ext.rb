3795bc0156d7480c9bed589afec631a73bac9dc6
Benjamin R. Haskell <ruby@benizi.com>
2012-10-25 09:29:02 -0400

    Safer monkey-patching of Pry.start/.teardown
    Allows other plugins to do the same without triggering a
    SystemStackError.

--- lib/pry-nav/pry_ext.rb
+++ lib/pry-nav/pry_ext.rb
@@ -2,19 +2,21 @@ require 'pry'
 require 'pry-nav/tracer'
 
 class << Pry
-  alias_method :start_existing, :start
+  alias_method :start_without_pry_nav, :start
 
-  def start(target = TOPLEVEL_BINDING, options = {})
+  def start_with_pry_nav(target = TOPLEVEL_BINDING, options = {})
     old_options = options.reject { |k, _| k == :pry_remote }
 
     if target.is_a?(Binding) && PryNav.check_file_context(target)
       # Wrap the tracer around the usual Pry.start
       PryNav::Tracer.new(options).run do
-        start_existing(target, old_options)
+        start_without_pry_nav(target, old_options)
       end
     else
       # No need for the tracer unless we have a file context to step through
-      start_existing(target, old_options)
+      start_without_pry_nav(target, old_options)
     end
   end
+
+  alias_method :start, :start_with_pry_nav
 end
